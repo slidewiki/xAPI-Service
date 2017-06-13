95773d87538fb5116d1754fec825439204ea21ce
Ben Wulff <benjamin.wulff@iais.fraunhofer.de>
2017-06-13 04:25:20 +0200

    Fixing image building

--- travis_scripts/dockerhub.sh
+++ travis_scripts/dockerhub.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD"
-docker build -t slidewiki/xapiservice ./
+docker build -t slidewiki/xapiservice:latest-dev ./
 docker push slidewiki/xapiservice:latest-dev
